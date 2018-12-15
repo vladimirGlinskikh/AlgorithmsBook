b746c0dc17f438e4b3dca3c25aee014959d887e0
Vladimir Glinskikh <support@zhelezyaka.kz>
2018-12-15 19:21:09 +0600

    add method setUp

--- src/test/java/bank/BankAccountTest.java
+++ src/test/java/bank/BankAccountTest.java
@@ -1,25 +1,32 @@
 package bank;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class BankAccountTest {
+    BankAccount bankAccount = null;
+
+    @Before
+    public void setUp() {
+        bankAccount = new BankAccount(100);
+    }
+
     @Test
     public void addDeposit() {
-        BankAccount bankAccount = new BankAccount(100.00);
-        assertEquals(174.35, bankAccount.deposit(74.35), 0.0);
+        assertEquals(174.35,
+                bankAccount.deposit(74.35), 0.0);
     }
 
     @Test
     public void minusDeposit() {
-        BankAccount bankAccount = new BankAccount(100.00);
-        assertEquals(50.00, bankAccount.withDraw(50.00), 0.0);
+        assertEquals(50.00,
+                bankAccount.withDraw(50.00), 0.0);
     }
 
     @Test
     public void depositNotNull() {
-        BankAccount bankAccount = new BankAccount(0.00);
         assertNotNull(bankAccount);
     }
 }
